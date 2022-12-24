da6eaf37b3891c74c0f5465abbad039abd33c370
序列号 <2402324010@qq.com>
2022-12-24 17:18:28 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,3 +16,23 @@
 # Add a feed source
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+
+# Remove packages
+rm -rf ./feeds/luci/applications/luci-app-passwall
+rm -rf ./feeds/packages/net/mosdns
+rm -rf ./feeds/packages/net/v2ray-geodata
+rm -rf ./package/feeds/luci/luci-app-passwall
+rm -rf ./package/feeds/packages/mosdns
+rm -rf ./package/feeds/packages/v2ray-geodata
+
+# Add packages
+svn co https://github.com/ophub/luci-app-amlogic/trunk package/amlogic
+
+# => ssr plus
+git clone --depth=1 https://github.com/fw876/helloworld.git package/helloworld
+# => 阿里ddns
+git clone https://github.com/chenhw2/luci-app-aliddns.git package/luci-app-aliddns
+# => 阿里云web dav
+git clone https://github.com/messense/aliyundrive-webdav.git package/luci-app-aliyundrive-webdav
+# => adguardhome
+git clone https://github.com/kongfl888/luci-app-adguardhome.git package/luci-app-adguardhome
